e03cce1c5abfc0c84ae96c8626f7cde9dd60bed1
linjilei <linjilei@qq.com>
2019-02-03 00:49:30 +0800

    完成服务端.

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -4,25 +4,51 @@ import { Post, User, Users, Posts } from './data'
 interface Args {
   id?: number;
   nickname?: string;
+  content?: string;
+  title?: string;
+  userId?: number;
 }
+interface PostArgs {
+  post: Post;
+}
+interface UserArgs {
+  user: User;
+}
+interface IdArgs {
+  id: number;
+}
+interface MutationResponse<T = any> {
+  code: number;
+  message: string;
+  data?: T;
+}
+
+function success <Data> (data: Data, message: string = '成功'): MutationResponse<Data> {
+  return {
+    code: 0,
+    message,
+    data
+  }
+}
+function error <Data> (data: Data, message: string = '失败'): MutationResponse<Data> {
+  return {
+    code: 1,
+    message,
+    data
+  }
+}
+
 interface Parent {
 }
 
 const resolvers: IResolvers = {
   Query: {
     posts () {
-      return Posts.map(post => {
-        post.user = Users.find(user => user.id === post.userId)
-        return post
-      })
+      return Posts
     },
-
     post (parent: Parent, args: Args) {
-      const post = <Post>Posts.find(post => post.id === args.id)
-      post.user = Users.find(user => user.id === post.userId)
-      return post
+      return <Post>Posts.find(post => post.id === args.id)
     },
-
     async users () {
       return Users
     },
@@ -35,6 +61,67 @@ const resolvers: IResolvers = {
         if (args.nickname) return user.nickname === args.nickname
         return false
       })
+    }
+  },
+  Post: {
+    async user (Post: Post, args: Args, context: any, info: any) {
+      return Users.find(user => user.id === Post.userId)
+    }
+  },
+  User: {
+    async posts (user: User) {
+      return Posts.filter(post => post.userId === user.id)
+    }
+  },
+  Mutation: {
+    addPost (parent: any, args: PostArgs): MutationResponse<Post> {
+      const post = {
+        id: Posts.length + 1,
+        title: <string>args.post.title,
+        content: <string>args.post.content,
+        userId: 2
+      }
+      Posts.push(post)
+      return success(post)
+    },
+    updatePost (parent: any, args: PostArgs): MutationResponse<Post | object> {
+      const post = Posts.find(({ id }) => id === args.post.id)
+      if (post) {
+        post.title = <string>args.post.title
+        post.content = <string>args.post.content
+        return success(post)
+      } else {
+        return error({})
+      }
+    },
+    async delPost (parent: any, args: IdArgs): Promise<MutationResponse> {
+      const index = Posts.findIndex(post => post.id === args.id)
+      Posts.splice(index, 1)
+      return success({})
+    },
+    async addUser (parent: any, args: UserArgs): Promise<MutationResponse<User>> {
+      const user = {
+        id: Users.length + 1,
+        nickname: <string>args.user.nickname,
+        age: <number>args.user.age
+      }
+      Users.push(user)
+      return success(user)
+    },
+    async updateUser (parent: any, args: UserArgs): Promise<MutationResponse<User | object>> {
+      const user = Users.find(({ id }) => id === args.user.id)
+      if (user) {
+        user.nickname = <string>args.user.nickname
+        user.age = <number>args.user.age
+        return success(user)
+      } else {
+        return error({})
+      }
+    },
+    async delUser (parent: any, args: IdArgs): Promise<MutationResponse> {
+      const index = Users.findIndex(user => user.id === args.id)
+      Users.splice(index, 1)
+      return success({})
     }
   }
 }
--- server/typeDefs.ts
+++ server/typeDefs.ts
@@ -1,6 +1,7 @@
 import { gql } from 'apollo-server'
 
 export default gql`
+  # 注释
   "用户"
   type User {
     id: Int!
@@ -8,6 +9,8 @@ export default gql`
     nickname: String
     "年龄"
     age: Int!
+    "发表过的文章"
+    posts: [Post]
   }
   "文章"
   type Post {
@@ -28,5 +31,71 @@ export default gql`
     users: [User]
     "按ID或昵称获取用户信息"
     user (nickname: String, id: Int): User
+  }
+
+  interface MutationResponse {
+    code: Int
+    message: String
+  }
+  input PostInput {
+    id: Int
+    "标题"
+    title: String
+    "内容"
+    content: String
+    "用户ID"
+    userId: Int
+  }
+
+  type AddPostMutationResponse implements MutationResponse {
+    code: Int
+    message: String
+    data: Post
+  }
+  type UpdatePostMutationResponse implements MutationResponse {
+    code: Int
+    message: String
+    data: Post
+  }
+  type DelPostMutationResponse implements MutationResponse {
+    code: Int
+    message: String
+  }
+  type AddUserMutationResponse implements MutationResponse {
+    code: Int
+    message: String
+    data: User
+  }
+  type UpdateUserMutationResponse implements MutationResponse {
+    code: Int
+    message: String
+    data: User
+  }
+  type DelUserMutationResponse implements MutationResponse {
+    code: Int
+    message: String
+  }
+
+  input UserInput {
+    id: Int
+    "昵称"
+    nickname: String
+    "年龄"
+    age: Int
+  }
+
+  type Mutation {
+    "添加文章"
+    addPost (post: PostInput): AddPostMutationResponse
+    "修改文章"
+    updatePost (post: PostInput): UpdatePostMutationResponse
+    "删除文章"
+    delPost (id: Int): DelPostMutationResponse
+    "添加用户"
+    addUser (user: UserInput): AddUserMutationResponse
+    "修改用户"
+    updateUser (user: UserInput): UpdateUserMutationResponse
+    "删除用户"
+    delUser (id: Int): DelUserMutationResponse
   }
 `
