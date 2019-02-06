dc5b52593ff6627bb90570388859ac882ed74411
linjilei <linjilei@qq.com>
2019-02-07 00:11:38 +0800

    添加详情显示

--- src/post.js
+++ src/post.js
@@ -4,12 +4,30 @@ import { fetchPost } from './api'
 import { Link } from './router'
 
 export default class Post extends React.Component {
-  componentDidMount () {
-    fetchPost(1)
+  constructor () {
+    super()
+    this.state = {
+      id: -1,
+      title: '',
+      content: '',
+      user: {
+        id: -1,
+        nickname: ''
+      }
+    }
+  }
+  async componentDidMount () {
+    const { data } = await fetchPost(Number(this.props.id))
+    this.setState(data.post)
   }
   render () {
+    const { title, content, user } = this.state
     return (
-      <div></div>
+      <div>
+        <h1>{title}</h1>
+        作者: <Link name='user' id={user.id}>{user.nickname}</Link>
+        <p>{content}</p>
+      </div>
     )
   }
 }
--- src/posts.js
+++ src/posts.js
@@ -6,7 +6,7 @@ import { Link } from './router'
 const Post = props => {
   const { data } = props
   return (
-    <div>
+    <div style={{ borderBottom: '1px solid #eee' }}>
       <h3>标题: {data.title}</h3>
       <h4>作者: <Link name="user" id={data.user.id}>{data.user.nickname}</Link></h4>
       <Link name="book" id={data.id}>点击查看详情</Link>
